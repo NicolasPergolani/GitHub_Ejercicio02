28a9ad9f0245248b4bd56f9aca6ef3cd55d9c448
diego picco <dpicco91@hotmail.com>
2023-02-16 21:42:16 -0300

    actualiseishon2

--- src/Entidades/Perro.java
+++ src/Entidades/Perro.java
@@ -13,6 +13,64 @@ public class Perro {
     private String raza,color,sexo;
     private int edad;
     private double peso;
+
+    public Perro() {
+    }
+
+    public Perro(String raza, String color, String sexo, int edad, double peso) {
+        this.raza = raza;
+        this.color = color;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.peso = peso;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "raza=" + raza + ", color=" + color + ", sexo=" + sexo + ", edad=" + edad + ", peso=" + peso + '}';
+    }
+    
+    
     
     
     
