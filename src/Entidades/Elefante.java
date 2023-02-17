d497149bf9b0fcec47f8cb76d846a52348b55614
Matias2189 <matias_2189@hotmail.com>
2023-02-16 21:19:52 -0300

    Subiendo entidad elefante

---
+++ src/Entidades/Elefante.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author MATIAS
+ */
+public class Elefante {
+ 
+    private String color;
+    private int edad;
+    private int peso;
+    private String nombre;
+           
+}
