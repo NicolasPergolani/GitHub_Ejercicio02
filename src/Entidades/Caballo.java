2b03ef6a43b290ccb6f4c32e3516765d7afe4146
Nico Pergo <nicopergo@gmail.com>
2023-02-16 21:06:56 -0300

    Commit Con Caballo

---
+++ src/Entidades/Caballo.java
@@ -0,0 +1,15 @@
+
+package Entidades;
+
+public class Caballo {
+    
+    private String color;
+    private int peso;
+    private String raza;
+    private String largoPelo;
+    private int herradura;
+    private boolean riendas;
+    
+    
+    
+}
